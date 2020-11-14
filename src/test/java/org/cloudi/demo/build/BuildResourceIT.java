6abe6a83278fc8d29d50197c97c510ccb58b9894
geekmee <geekmee@sohu.com>
2020-11-14 22:33:57 +0800

    clean imports

--- src/test/java/org/cloudi/demo/build/BuildResourceIT.java
+++ src/test/java/org/cloudi/demo/build/BuildResourceIT.java
@@ -1,7 +1,6 @@
 package org.cloudi.demo.build;
 
 import io.quarkus.test.junit.NativeImageTest;
-import org.cloudi.demo.build.BuildResourceTest;
 
 @NativeImageTest
 public class BuildResourceIT extends BuildResourceTest {
